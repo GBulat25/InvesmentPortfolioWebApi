4d5383b74ae14ab99748e6d64aa5459c7ac1d249
Bulat Gazizyanov <gbulat882@gmail.com>
2025-06-15 13:58:01 +0300

    установка ef core

---
+++ Data/StockDBContext.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.EntityFrameworkCore;
+using StocksWebApi.Models;
+using System.Security.Cryptography.X509Certificates;
+namespace StocksWebApi.Data
+{
+    public class StockDBContext: DbContext
+    {
+        public StockDBContext(DbContextOptions options): base(options) 
+        {
+            
+        }
+        public DbSet<Stock> Stocks { get; set; }
+        public DbSet<Comment> Comments { get; set; }
+    }
+}
