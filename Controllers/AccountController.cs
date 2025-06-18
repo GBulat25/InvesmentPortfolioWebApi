5e458fa8f522e1115c9a2fef72a7c00ed2527d42
Bulat Gazizyanov <gbulat882@gmail.com>
2025-06-18 13:10:30 +0300

    добавление авторизации

--- Controllers/AccountController.cs
+++ Controllers/AccountController.cs
@@ -1,5 +1,6 @@
 ﻿using Microsoft.AspNetCore.Identity;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
 using Microsoft.OpenApi.Validations;
 using StocksWebApi.DTOs.Account;
 using StocksWebApi.Interfaces;
@@ -13,10 +14,38 @@ namespace StocksWebApi.Controllers
     {
         private readonly UserManager<AppUser> _userManager;
         private readonly ITokenService _tokenService;
-        public AccountController(UserManager<AppUser> userManager, ITokenService tokenService)
+        private readonly SignInManager<AppUser> _signInManager;
+        public AccountController(UserManager<AppUser> userManager, ITokenService tokenService, SignInManager<AppUser> signInManager)
         {
             _userManager = userManager;
             _tokenService = tokenService;
+            _signInManager = signInManager;
+        }
+        [HttpPost("login")]
+        public async Task<IActionResult> Login(LoginDTO loginDto)
+        {
+            if (!ModelState.IsValid)
+            {
+                return BadRequest(ModelState);
+            }
+            var user = await _userManager.Users.FirstOrDefaultAsync(x => x.UserName == loginDto.UserName);
+            if (user == null)
+            {
+                return Unauthorized("Invalid Username");
+            }
+            var result= await _signInManager.CheckPasswordSignInAsync(user, loginDto.Password,false);
+            if (!result.Succeeded)
+            {
+                return Unauthorized("Usernamenot found and/or password incorrect");
+            }
+            return Ok(
+                        new NewUserDTO
+                        {
+                            UserName = user.UserName,
+                            Email = user.Email,
+                            Token = _tokenService.CreateToken(user)
+                        }
+                      );
         }
         [HttpPost("register")]
         public async Task<IActionResult> Register([FromBody] RegisterDTO registerDto)
